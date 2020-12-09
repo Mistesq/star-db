39778f2571e73d0091ad816a3b233b005ca910d6
Mistesqr <mistesqr@gmail.com>
2020-12-09 18:41:29 +0200

    random-planet upd && added spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.scss';
+
+export default function Spinner() {
+    return (
+            <div className="spinner">
+                <div className="loadingio-spinner-spinner-3fc7xbbtn1q">
+                    <div className="ldio-v2jdul58ck">
+                        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+                    </div>
+                </div>
+            </div>
+    )
+}
